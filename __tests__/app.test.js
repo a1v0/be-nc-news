7eaf369143f3afa5e1ada511c2716276ccc1cc6f
avc285 <avc285@gmail.com>
2022-11-14 11:22:00 +0000

    import all required elements into test suite

--- __tests__/app.test.js
+++ __tests__/app.test.js
@@ -1,1 +1,35 @@
-describe("", {});
+const request = require("supertest");
+const app = require("../app.js");
+
+const db = require("../db/connection.js");
+const seed = require("../db/seeds/seed.js");
+
+const {
+    articleData,
+    commentData,
+    topicData,
+    userData
+} = require("../db/data/test-data/index.js");
+
+beforeEach(() => {
+    return seed({ topicData, userData, articleData, commentData });
+});
+afterAll(() => {
+    db.end();
+});
+
+describe("/api/topics", () => {
+    test.todo("");
+    test.todo("");
+    test.todo("");
+    test.todo("");
+    test.todo("");
+    test.todo("");
+    test.todo("");
+    test.todo("");
+    test.todo("");
+    test.todo("");
+    test.todo("");
+    test.todo("");
+    test.todo("");
+});
