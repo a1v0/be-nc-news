41d8c8e03b91e0ad58cfe2d3871cd0c16d83002e
avc285 <avc285@gmail.com>
2022-11-17 14:08:13 +0000

    combine multiple endpoints with same path using .route().get()... syntax

--- app.js
+++ app.js
@@ -26,11 +26,13 @@ app.get("/api/topics", getTopics);
 
 app.get("/api/articles", getArticles);
 
-app.get("/api/articles/:article_id", getArticleById);
-app.patch("/api/articles/:article_id", patchArticleById);
+app.route("/api/articles/:article_id")
+    .get(getArticleById)
+    .patch(patchArticleById);
 
-app.get("/api/articles/:article_id/comments", getCommentsByArticleId);
-app.post("/api/articles/:article_id/comments", postCommentByArticleId);
+app.route("/api/articles/:article_id/comments")
+    .get(getCommentsByArticleId)
+    .post(postCommentsByArticleId);
 
 app.get("/api/users", getUsers);
 
